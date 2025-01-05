bb9ee11360373956fc92da0a50a9d086acb74e4b
1518manu <manudev1518@gmail.com>
2025-01-05 18:42:40 +0530

    c

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import SiteHeader from "@/components/site-header"
-// import { Children } from "react";
+import SiteHeader from "@/components/site-header";
+
 export default function Home() {
   return (
-   <div>
-    <SiteHeader/>
-    {/* {children} */}
+    <div>
+      <SiteHeader />
     </div>
   );
 }
